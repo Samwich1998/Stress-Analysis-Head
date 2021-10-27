fd422d89f976fe689efe88a562cb822b3c5b7c7c
Samuel Solomon <sasolomon1998@gmail.com>
2021-10-27 16:03:35 -0700

    Workin Calibration

--- Helper Files/eogControl.py
+++ Helper Files/eogControl.py
@@ -49,10 +49,10 @@ if __name__ == "__main__":
 
     # General Data Collection Information (You Will Likely Not Edit These)
     eogSerialNum = '85035323234351D06052'#'85035323234351D06052'   # Arduino's Serial Number (port.serial_number)
-    samplingFreq = 990           # The Average Number of Points Steamed Into the Arduino Per Second; If NONE Given, Algorithm will Calculate Based on Initial Data
-    numDataPoints = 6000         # The Number of Points to Stream into the Arduino
+    samplingFreq = None           # The Average Number of Points Steamed Into the Arduino Per Second; If NONE Given, Algorithm will Calculate Based on Initial Data
+    numDataPoints = 100000         # The Number of Points to Stream into the Arduino
     numTimePoints = 5000          # The Number of Data Points to Display to the User at a Time; My beta-Test Used 2000 Points
-    moveDataFinger = 100          # The Number of Data Points to Plot/Analyze at a Time; My Beta-Test Used 200 Points
+    moveDataFinger = 5          # The Number of Data Points to Plot/Analyze at a Time; My Beta-Test Used 200 Points
     numChannels = 2               # The Number of Arduino Channels with EOG Signals Read in; My Beta-Test Used 4 Channels
      
     # Protocol Switches: Only the First True Variable Excecutes
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     plotStreamedData = False      # Graph the Data to Show Incoming Signals + Analysis
     calibrateModel = True         # Calibrate the EOG Voltage to Predict the Eye's Angle
     saveInputData = False         # Saves the Data in 'readData.data' in an Excel Named 'saveExcelName'
-    controlVR = False             # Apply the Algorithm to Control the Virtual Reality View
+    controlVR = True             # Apply the Algorithm to Control the Virtual Reality View
     
     # ---------------------------------------------------------------------- #
     

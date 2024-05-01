eae51861db357fdcb92b4c33138546f001150adc
Samuel Solomon <sasolomon1998@gmail.com>
2024-04-30 20:56:27 -0700

    Updating

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import time
+import random
 import warnings
 import argparse
 import numpy as np
@@ -35,6 +36,13 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Suppress specific PyTorch warnings about MPS fallback
 warnings.filterwarnings("ignore", message="The operator 'aten::linalg_svd' is not currently supported on the MPS backend and will fall back to run on the CPU.")
+
+# Use the same initialization every time.
+torch.use_deterministic_algorithms(True)
+torch.set_default_dtype(torch.float32)
+torch.manual_seed(0)  # For PyTorch
+np.random.seed(0)
+random.seed(0)
 
 if __name__ == "__main__":
     # Define the accelerator parameters.

0d343385b152c72397033a49919044a30e44482f
Samuel Solomon <sasolomon1998@gmail.com>
2024-05-03 11:28:55 -0700

    Updating

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
@@ -106,7 +106,7 @@ class emotionPipeline:
         # Common WD values: 1E-2 to 1E-6
         modelParams = [
             # Specify the model parameters for the signal encoding.
-            {'params': signalEncoderModel.parameters(), 'weight_decay': 1E-4, 'lr': 5E-4 if self.fullTest else 5E-4}]
+            {'params': signalEncoderModel.parameters(), 'weight_decay': 1E-2, 'lr': 5E-4 if self.fullTest else 5E-4}]
         if submodel in ["autoencoder", "emotionPrediction"]:
             modelParams.append(
                 # Specify the model parameters for the autoencoder.
@@ -347,7 +347,7 @@ class emotionPipeline:
                     # Calculate the gradients.
                     self.accelerator.backward(finalLoss)  # Calculate the gradients.
                     t2 = time.time(); self.accelerator.print(f"Backprop {self.datasetName} {numPointsAnalyzed}:", t2 - t1)
-                    if self.accelerator.sync_gradients: self.accelerator.clip_grad_norm_(self.model.parameters(), 3)  # Apply gradient clipping: Small: <1; Medium: 5-10; Large: >20
+                    if self.accelerator.sync_gradients: self.accelerator.clip_grad_norm_(self.model.parameters(), 1)  # Apply gradient clipping: Small: <1; Medium: 5-10; Large: >20
                     # Backpropagation the gradient.
                     self.optimizer.step()  # Adjust the weights.
                     self.optimizer.zero_grad()  # Zero your gradients to restart the gradient tracking.

a924b2da0bdcc1794a9e4efad664912a3d895a6c
Sammy <sasolomon1998@gmail.com>
2024-05-06 23:29:04 -0700

    U

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderHelpers/signalEncoderModules.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderHelpers/signalEncoderModules.py
@@ -59,21 +59,21 @@ class signalEncoderModules(convolutionalHelpers):
 
     def neuralWeightParameters(self, inChannel=1, outChannel=2, secondDimension=46):
         # Corrected calculation for the standard deviation
-        fan_in = inChannel  # Ensure division is performed before multiplication
+        fan_in = inChannel*outChannel  # Ensure division is performed before multiplication
 
         # Initialize the weights with a normal distribution.
         parameter = nn.Parameter(torch.randn((outChannel, inChannel, secondDimension)))
-        parameter = self.modelHelpers.lecunParamInitialization(parameter, fan_in)
+        parameter = self.modelHelpers.heUniformInit(parameter, fan_in)
 
         return parameter
 
     def neuralCombinationWeightParameters(self, inChannel=1, initialFrequencyDim=2, finalFrequencyDim=1):
         # Corrected calculation for the standard deviation
-        fan_in = initialFrequencyDim  # Ensure division is performed before multiplication
+        fan_in = initialFrequencyDim*inChannel  # Ensure division is performed before multiplication
 
         # Initialize the weights with a normal distribution.
         parameter = nn.Parameter(torch.randn((inChannel, initialFrequencyDim, finalFrequencyDim)))
-        parameter = self.modelHelpers.lecunParamInitialization(parameter, fan_in)
+        parameter = self.modelHelpers.heUniformInit(parameter, fan_in)
 
         return parameter
 

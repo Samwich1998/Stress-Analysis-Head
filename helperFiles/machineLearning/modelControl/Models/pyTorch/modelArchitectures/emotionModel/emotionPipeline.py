a5fb81ba8e5232ab60fdd1b622e0db7efee6e361
Sammy <sasolomon1998@gmail.com>
2024-05-12 20:14:42 -0700

    FINAL

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
@@ -417,7 +417,7 @@ class emotionPipeline:
     def setOptimizer(self, params, lr, weight_decay, submodel):
         if submodel == "signalEncoder":
             # RAdam is bad; AdamW is good;
-            return self.getOptimizer(optimizerType="Adagrad", params=params, lr=lr, weight_decay=weight_decay)
+            return self.getOptimizer(optimizerType="Adam", params=params, lr=lr, weight_decay=weight_decay)
         elif submodel == "autoencoder":
             return optim.AdamW(params, lr=lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False, maximize=False)
         elif submodel == "emotionPrediction":

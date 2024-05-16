960de9b75ba963429264a2706a2fb1aa5662f1f0
Sammy <sasolomon1998@gmail.com>
2024-05-15 23:46:29 -0700

    FINAL

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
@@ -219,11 +219,11 @@ class emotionPipeline:
                         finalLoss = signalReconstructedLoss
 
                         # Compile the loss into one value
-                        if 0.25 < encodedSignalStandardDeviationLoss:
+                        if 0.3 < encodedSignalStandardDeviationLoss:
                             finalLoss = finalLoss + 0.1 * encodedSignalStandardDeviationLoss
                         if 0.001 < signalEncodingTrainingLayerLoss:
                             finalLoss = finalLoss + 0.5*signalEncodingTrainingLayerLoss
-                        if 0.25 < encodedSignalMeanLoss:
+                        if 0.3 < encodedSignalMeanLoss:
                             finalLoss = finalLoss + 0.1 * encodedSignalMeanLoss
                         # Account for the current training state when calculating the loss.
                         finalLoss = compressionFactor * noiseFactor * sequenceLengthFactor * futureCompressionsFactor * finalLoss

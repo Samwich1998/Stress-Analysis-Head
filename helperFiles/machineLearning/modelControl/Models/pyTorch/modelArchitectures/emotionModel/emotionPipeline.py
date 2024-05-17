f324301d9f9aa5a77fcbb89d47fd8d5bcd18765b
Sammy <sasolomon1998@gmail.com>
2024-05-17 00:42:41 -0700

    FINAL

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModel/emotionPipeline.py
@@ -220,11 +220,11 @@ class emotionPipeline:
                         finalLoss = compressionFactor*signalReconstructedLoss
 
                         # Compile the loss into one value
-                        if (finalLoss < 0.1 and 0.2 < encodedSignalStandardDeviationLoss) or 0.3 < encodedSignalStandardDeviationLoss:
+                        if (finalLoss < 0.1 and 0.25 < encodedSignalStandardDeviationLoss) or 0.75 < encodedSignalStandardDeviationLoss:
                             finalLoss = finalLoss + 0.1*encodedSignalStandardDeviationLoss
                         if 0.001 < signalEncodingTrainingLayerLoss:
                             finalLoss = finalLoss + 0.75*signalEncodingTrainingLayerLoss
-                        if (finalLoss < 0.1 and 0.2 < encodedSignalMeanLoss) or 0.3 < encodedSignalMeanLoss:
+                        if (finalLoss < 0.1 < encodedSignalMeanLoss) or 0.2 < encodedSignalMeanLoss:
                             finalLoss = finalLoss + 0.1*encodedSignalMeanLoss
                         # Account for the current training state when calculating the loss.
                         finalLoss = noiseFactor*sequenceLengthFactor*futureCompressionsFactor * finalLoss
--- slurmScripts/signalEncoder/runScript.sh
+++ slurmScripts/signalEncoder/runScript.sh
@@ -15,18 +15,20 @@ numEncodingLayersStep=-1
 numEncodingLayersEnd=0      # Memory limited from 10-12.
 # Total: 9
 
+optimizer="NAdam"
+
 for numLiftedChannels in $(seq $numLiftedChannelsStart $numLiftedChannelsStep $numLiftedChannelsEnd)
 do
   for numExpandedSignals in $(seq $numExpandedSignalsStart $numExpandedSignalsStep $numExpandedSignalsEnd)
   do
       for numEncodingLayers in $(seq $numEncodingLayersStart $numEncodingLayersStep $numEncodingLayersEnd)
       do
-          echo "Submitting job with $numLiftedChannels numLiftedChannels and $numExpandedSignals numExpandedSignals and $numEncodingLayers numEncodingLayers on $1 with AdamW"
+          echo "Submitting job with $numLiftedChannels numLiftedChannels and $numExpandedSignals numExpandedSignals and $numEncodingLayers numEncodingLayers on $1 with $optimizer"
 
           if [ "$1" == "CPU" ]; then
-              sbatch -J "signalEncoder_numLift_${numLiftedChannels}_numExp${numExpandedSignals}_numEnc${numEncodingLayers}_$1" submitSignalEncoder_CPU.sh "$numLiftedChannels" "$numExpandedSignals" "$numEncodingLayers" "$1" "AdamW"
+              sbatch -J "signalEncoder_numLift_${numLiftedChannels}_numExp${numExpandedSignals}_numEnc${numEncodingLayers}_$1" submitSignalEncoder_CPU.sh "$numLiftedChannels" "$numExpandedSignals" "$numEncodingLayers" "$1" "$optimizer"
           elif [ "$1" == "GPU" ]; then
-              sbatch -J "signalEncoder_numLift_${numLiftedChannels}_numExp${numExpandedSignals}_numEnc${numEncodingLayers}_$1" submitSignalEncoder_GPU.sh "$numLiftedChannels" "$numExpandedSignals" "$numEncodingLayers" "$1" "AdamW"
+              sbatch -J "signalEncoder_numLift_${numLiftedChannels}_numExp${numExpandedSignals}_numEnc${numEncodingLayers}_$1" submitSignalEncoder_GPU.sh "$numLiftedChannels" "$numExpandedSignals" "$numEncodingLayers" "$1" "$optimizer"
           else
               echo "No known device listed: $1"
           fi

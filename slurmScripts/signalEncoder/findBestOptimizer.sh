c5a4bba29282812b24ae126026e404199966bc62
Samwich1998 <sasolomon1998@gmail.com>
2024-05-17 19:35:35 -0700

    U

--- slurmScripts/signalEncoder/findBestOptimizer.sh
+++ slurmScripts/signalEncoder/findBestOptimizer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 optimizers=('Adadelta' 'Adam' 'AdamW' 'NAdam' 'RAdam' 'Adamax' 'ASGD' 'RMSprop' 'Rprop' 'SGD')
-numLiftedChannels=32
+numLiftedChannels=64
 numExpandedSignals=2
 numEncodingLayers=2
 

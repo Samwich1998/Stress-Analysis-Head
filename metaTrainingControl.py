6c3633c8547b28316c6f8d0637548bee5380f0cf
Sammy <sasolomon1998@gmail.com>
2024-05-17 19:24:14 -0700

    FINAL

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     accelerator = accelerate.Accelerator(
         dataloader_config=DataLoaderConfiguration(split_batches=True),  # Whether to split batches across devices or not.
         step_scheduler_with_optimizer=False,  # Whether to wrap the optimizer in a scheduler.
-        gradient_accumulation_steps=2,  # The number of gradient accumulation steps.
+        gradient_accumulation_steps=8,  # The number of gradient accumulation steps.
         mixed_precision="no",  # FP32 = "no", BF16 = "bf16", FP16 = "fp16", FP8 = "fp8"
     )
 

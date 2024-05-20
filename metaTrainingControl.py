42b70e02e5c23d244bc1457c0cd91ef2e0555c03
Sammy <sasolomon1998@gmail.com>
2024-05-19 21:57:09 -0700

    FINAL

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
             if args.numLiftedChannels <= 32 and args.numEncodingLayers <= 2:
                 accelerator.gradient_accumulation_steps = 16
             if args.numLiftedChannels <= 16 and args.numEncodingLayers <= 2:
-                accelerator.gradient_accumulation_steps = 8
+                accelerator.gradient_accumulation_steps = 16
 
         print("HPC Parameters:", storeLoss, fastPass, accelerator.gradient_accumulation_steps, flush=True)
 

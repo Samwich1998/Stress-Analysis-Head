855c6c33a845973a73aa10d68dde3f839c048560
Sammy <sasolomon1998@gmail.com>
2024-05-23 20:47:58 -0700

    FINAL

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderHelpers/signalEncoderModules.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderHelpers/signalEncoderModules.py
@@ -44,6 +44,9 @@ class signalEncoderModules(convolutionalHelpers):
             self.weightInitialization.initialize_weights(nn.Linear(numFeatures, numFeatures), activationMethod='boundedExp', layerType='fc'),
             boundedExp(),
 
+            self.weightInitialization.initialize_weights(nn.Linear(numFeatures, numFeatures), activationMethod='boundedExp', layerType='fc'),
+            boundedExp(),
+
             self.weightInitialization.initialize_weights(nn.Linear(numFeatures, numClasses), activationMethod='none', layerType='fc'),
             nn.Softmax(dim=-1),
         )

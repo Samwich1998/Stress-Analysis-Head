800254a4d8c42e59327c7df2117259bf09a6ef29
Sammy <sasolomon1998@gmail.com>
2024-05-23 19:26:24 -0700

    FINAL

--- metaTrainingControl.py
+++ metaTrainingControl.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     )
 
     # General model parameters.
-    trainingDate = "2024-05-22"  # The current date we are training the model. Unique identifier of this training set.
+    trainingDate = "2024-05-23"  # The current date we are training the model. Unique identifier of this training set.
     modelName = "emotionModel"  # The emotion model's unique identifier. Options: emotionModel
     testSplitRatio = 0.2  # The percentage of testing points.
 

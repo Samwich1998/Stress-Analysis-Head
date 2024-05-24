ea1a81870f158d3f1fc95f47896138c128eb75bf
Sammy <sasolomon1998@gmail.com>
2024-05-23 22:18:26 -0700

    FINAL

--- helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderHelpers/signalEncoderModules.py
+++ helperFiles/machineLearning/modelControl/Models/pyTorch/modelArchitectures/emotionModelInterface/emotionModel/emotionModelHelpers/submodels/modelComponents/signalEncoderHelpers/signalEncoderModules.py
@@ -102,6 +102,7 @@ class signalEncoderModules(convolutionalHelpers):
 
             # Convolution architecture: residual connection, feature engineering
             self.convolutionalFilters_resNetBlocks(numResNets=2, numBlocks=4, numChannels=[outChannel, outChannel], kernel_sizes=3, dilations=1, groups=1, strides=1, convType='conv1D', activationType='boundedExp', numLayers=None, useSwitchActivation=True),
+        )
 
     # ------------------- Final Statistics Architectures ------------------- #
 

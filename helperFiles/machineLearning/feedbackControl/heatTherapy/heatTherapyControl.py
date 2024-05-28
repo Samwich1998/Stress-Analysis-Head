f9a0f82bf6277f3df6e108ae0ad6ba7728c4bf50
samuel Solomon <samuelsolomon@samuels-MBP.lan>
2024-05-28 01:48:39 -0700

    HEAD

--- helperFiles/machineLearning/feedbackControl/heatTherapy/heatTherapyControl.py
+++ helperFiles/machineLearning/feedbackControl/heatTherapy/heatTherapyControl.py
@@ -3,59 +3,17 @@ import time
 import torch
 import sys
 
+# Spyder interface.
 sys.path.append("./../../../../")
 import helperFiles
 
 # Import the necessary libraries.
-from helperFiles.machineLearning.feedbackControl.heatTherapy.helperMethods.therapyProtcols.aStarProtocol import aStarProtocol
-from helperFiles.machineLearning.feedbackControl.heatTherapy.helperMethods.therapyProtcols.basicProtocol import basicProtocol
-from helperFiles.machineLearning.feedbackControl.heatTherapy.helperMethods.therapyProtcols.HMMProtocol import HMMProtocol
-from helperFiles.machineLearning.feedbackControl.heatTherapy.helperMethods.therapyProtcols.nnProtocol import nnProtocol
+from helperFiles.machineLearning.feedbackControl.heatTherapy.helperMethods.heatTherapyHelpers import heatTherapyHelpers
 
 
-class heatTherapyControl:
+class heatTherapyControl(heatTherapyHelpers):
     def __init__(self, userName, temperatureBounds, simulationParameters, therapyMethod="aStarProtocol", plotResults=False):
-        # General parameters.
-        self.simulationParameters = simulationParameters
-        self.temperatureBounds = temperatureBounds
-        self.plotResults = plotResults
-        self.userName = userName
-
-        # Therapy parameters.
-        self.therapyProtocol = None
-        self.therapyMethod = None
-
-        # Set up the therapy protocols.
-        self.setupTherapyProtocols(therapyMethod)
-
-        # for nn protocol, latest loss documentation
-        self.latestLoss = 1
-
-    def setUserName(self, userName):
-        self.userName = userName
-
-    def setupTherapyProtocols(self, therapyMethod):
-        # Change the therapy method.
-        self.therapyMethod = therapyMethod
-        if self.therapyMethod == "aStarProtocol":
-            self.therapyProtocol = aStarProtocol(self.temperatureBounds, self.simulationParameters, learningRate=2)
-        elif self.therapyMethod == "basicProtocol":
-            self.therapyProtocol = basicProtocol(self.temperatureBounds, self.simulationParameters)
-        elif self.therapyMethod == "nnProtocol":
-            self.therapyProtocol = nnProtocol(self.temperatureBounds, self.simulationParameters, modelName="2024-04-12 heatTherapyModel", onlineTraining=False)
-        elif self.therapyMethod == "HMMProtocol":
-            self.therapyProtocol = HMMProtocol(self.temperatureBounds, self.simulationParameters)
-        else:
-            raise ValueError("Invalid therapy method provided.")
-
-    def runHMMProtocol(self):
-        # train HMM model
-        self.therapyProtocol.train()
-        # predict the optimal state
-        # update the user state path
-        self.therapyProtocol.updateTherapyState()
-        # print out the optimal temperature and loss
-        print('Optimal temperature and loss:', self.therapyProtocol.userStatePath[-1])
+        super().__init__(userName=userName, temperatureBounds=temperatureBounds, simulationParameters=simulationParameters, therapyMethod=therapyMethod, plotResults=plotResults)
 
     def runTherapyProtocol(self, maxIterations=None):
         # Initialize holder parameters such as the user maps.
